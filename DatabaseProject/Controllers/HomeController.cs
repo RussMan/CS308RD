0ac07364cad10bf0b031e1591ee12faaaa1b82ba
RussMan <rbmcitywt@gmail.com>
2012-11-10 04:07:21 -0500

    First pull down from CS308RD/master to work on the latest files
    Signed-off-by: Russell Manlapat <rbmcitywt@gmail.com>

--- DatabaseProject/Controllers/HomeController.cs
+++ DatabaseProject/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace DatabaseProject.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Hello this is a modification!";
 
             return View();
         }
