640c129bd1b2b38a3d9a7441a2129f7f4c304330
RussMan <rbmcitywt@gmail.com>
2012-11-27 10:14:52 -0500

    Applied MD5 hashing to the user password during registration. Changed "About" page to "Public Posts" page. Applied two new styles of presentation - accordion and carousel -  for the posts in the Public Posts and Content pages.
    Signed-off-by: RussMan <rbmcitywt@gmail.com>

--- DatabaseProject/Controllers/HomeController.cs
+++ DatabaseProject/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DatabaseProject.Models;
 
 namespace DatabaseProject.Controllers
 {
@@ -15,11 +16,12 @@ namespace DatabaseProject.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult PublicPosts(int page = 0) //Temporary fix just to show the first 5 posts; TODO: allow for all type 0 posts to be shown
         {
-            ViewBag.Message = "Your app description page. Here is the featured section of the About page!";
-
-            return View();
+            //ViewBag.Message = "Your app description page. Here is the featured section of the About page!";
+            QueryController SQL = new QueryController();
+            PostListModel post_list = SQL.get_posts(page);
+            return View(post_list);
         }
 
         public ActionResult Contact()
